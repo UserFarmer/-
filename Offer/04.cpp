4a1fa07234077cef98fc0c414df8d4b340b432e6
IsKhayon <IsKhayon@outlook.com>
2022-03-20 15:49:58 +0800

    3.20

--- Offer/04.cpp
+++ Offer/04.cpp
@@ -23,36 +23,38 @@ using namespace std;
 
 class Solution {
 public:
-    bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
-        if(matrix.size() == 0)
-            return false;
+  bool findNumberIn2DArray(vector<vector<int>> &matrix, int target) {
+    if (matrix.size() == 0)
+      return false;
 
-        int x = 0;
-        int y = matrix.size() - 1;
+    int x = 0;
+    int y = matrix.size() - 1;
 
-        while(x < matrix[0].size() && y >= 0) {
-            if(matrix[y][x] > target) {
-                y--;
-            }
-            else if(matrix[y][x] < target) {
-                x++;
-            }
-            else if(matrix[y][x] == target) {
-                return true;
-            }
-        }
-        return false;
+    while (x < matrix[0].size() && y >= 0) {
+      if (matrix[y][x] > target) {
+        y--;
+      } else if (matrix[y][x] < target) {
+        x++;
+      } else if (matrix[y][x] == target) {
+        return true;
+      }
     }
+    return false;
+  }
 };
 
 int main() {
-    Solution s;
-    vector<vector<int>> matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
-    int target = 20;
+  Solution s;
+  vector<vector<int>> matrix = {{1, 4, 7, 11, 15},
+                                {2, 5, 8, 12, 19},
+                                {3, 6, 9, 16, 22},
+                                {10, 13, 14, 17, 24},
+                                {18, 21, 23, 26, 30}};
+  int target = 20;
 
-    bool ans = s.findNumberIn2DArray(matrix, target);
-    cout << boolalpha << ans << endl;
+  bool ans = s.findNumberIn2DArray(matrix, target);
+  cout << boolalpha << ans << endl;
 
-    system("pause");
-    return 0;
+  system("pause");
+  return 0;
 }
